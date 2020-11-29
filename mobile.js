cb9da2bd522d6824ba06e79ecda087c123b2452f
CodeBill <75174381+CodeBill@users.noreply.github.com>
2020-11-29 09:54:38 +0800

    Add files via upload

---
+++ mobile.js
@@ -0,0 +1,11 @@
+document.querySelector('.burgr')
+	.addEventListener('click', () =>{
+		document.getElementById("side").style.right = "0"
+
+});
+
+document.querySelector('.lni-close')
+	.addEventListener('click', () =>{
+		document.getElementById("side").style.right = "-300px"
+
+});
